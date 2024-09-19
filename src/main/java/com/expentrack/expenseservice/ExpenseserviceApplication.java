b17413821297dbf1d1b4e907fabb52db60c79e9f
sayanthp <psayanth.p@gmail.com>
2024-09-20 01:28:01 +0530

    Project created

---
+++ src/main/java/com/expentrack/expenseservice/ExpenseserviceApplication.java
@@ -0,0 +1,13 @@
+package com.expentrack.expenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expentrack/expenseservice/ExpenseserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expentrack.expenseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
